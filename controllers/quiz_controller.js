d7b0ae83df7ad8c408cf90dfaaa883f0e5ac849b
ignaciocolodron <ignacio.colodron.bestuer@gmail.com>
2015-08-14 14:17:39 +0200

    Añadido el campo tema a la entidad quiz
    Añadido el campo tema a la entidad quiz

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -47,7 +47,7 @@ exports.answer = function(req, res){
 // GET /quizes/new
 exports.new_quiz = function(req, res){
 	var quiz = models.Quiz.build(// crea objeto quiz
-		{pregunta:"Pregunta", respuesta:"Respuesta"}
+		{pregunta:"Pregunta", respuesta:"Respuesta", tema:"otro"}
 	);
 	
 	res.render('quizes/new', {quiz: quiz, errors: []});
@@ -66,7 +66,7 @@ exports.create = function(req, res){
 			}else{
 				// guarda en DB los campos pregunta y respuesta de quiz
 				quiz
-				.save({fields: ["pregunta", "respuesta"]})
+				.save({fields: ["pregunta", "respuesta", "tema"]})
 				.then(function(){
 					res.redirect("/quizes");
 				})  // Redirección HTTP	(URL relativo) lista de preguntas
@@ -91,6 +91,7 @@ exports.edit = function(req, res){
 exports.update = function(req, res) {
 	req.quiz.pregunta = req.body.quiz.pregunta;
 	req.quiz.respuesta = req.body.quiz.respuesta;
+	req.quiz.tema = req.body.quiz.tema;
 	
 	req.quiz
 	.validate()
@@ -100,7 +101,7 @@ exports.update = function(req, res) {
 				res.render("quizes/edit", {quiz: req.quiz, errors: err.errors});
 			}else{
 				req.quiz	// save: guarda campos pregunta y respuesta en DB
-				.save( {fields: ["pregunta", "respuesta"]})
+				.save( {fields: ["pregunta", "respuesta", "tema"]})
 				.then( function(){ res.redirect("/quizes");});
 			}		// Redirección HTTP a lista de preguntas (URL relativo)
 		}
